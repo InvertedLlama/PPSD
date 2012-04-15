6ef0db13b20414a8b3abb1a31ca0ec2be5619dac
cfehunter <cfehunter@gmail.com>
2012-04-15 06:48:19 +0100

    Forgot to add new files

---
+++ Phase 3 - Implementation/PPSDPart2/Objects/ValueTreeNode.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PPSDPart2
+{
+    public class ValueTreeNode : TreeNode
+    {
+        object mValue;        
+
+        public ValueTreeNode(string nodeText, object value) : base(nodeText)
+        {
+            mValue = value;
+        }
+
+        public object Value
+        {
+            get { return mValue; }
+            set { mValue = value; }
+        }
+
+        public Type Type
+        {
+            get { return mValue.GetType(); }
+        }
+    }
+}
